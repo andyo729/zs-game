026abd84c85998fb1e18080e5b25ebf950646286
Andy Orahoske <andrew.orahoske@zipscene.com>
2016-05-17 21:15:23 -0400

    total points and points this week

--- app/user/show/controller.js
+++ app/user/show/controller.js
@@ -1,6 +1,16 @@
+const { moment } = window;
+
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  pointsThisWeek: Ember.computed('model.user.points.@each.createdOn', function() {
+    const points = this.get('model.user.points').toArray();
+
+    return points.filter((point) => {
+      const createdOn = moment(point.get('createdOn'));
+      return createdOn.isSame(moment(), 'week');
+    });
+  }),
   actions: {
     addPoint(user, pointType) {
       const point = this.store.createRecord('point', {
