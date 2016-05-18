91ce4943a65dc97c190ee29149100ea82e99ca0f
Rob Hilgefort <rjhilgefort@gmail.com>
2016-05-17 22:14:43 -0400

    add bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+  var app = new EmberApp(defaults, {});
 
   app.import('bower_components/moment/min/moment.min.js');
 
